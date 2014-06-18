c8d4d5e55f98bcc8187da90c9607b11ccce13a1a
acesmndr <acesmndr@gmail.com>
2014-06-18 17:21:19 +0545

    RED Spreadsheet Bar Graph js and jquery library pushed

---
+++ js/main.js
@@ -0,0 +1,93 @@
+		var greyRenderer = function (instance, td, row, col, prop, value, cellProperties) {
+ 	 	Handsontable.renderers.TextRenderer.apply(this, arguments);
+  		$(td).css({
+    		background: '#ccc'
+  			});
+		};	              
+		 var $container = $("#example1");
+                var $console = $("#example1console");
+                var $parent = $container.parent();
+                var autosaveNotification;
+                $container.handsontable({
+                  startRows: 10,
+                  startCols: 8,
+                  rowHeaders: false,
+                  colHeaders: false,
+                  minSpareRows: 1,
+                  contextMenu: true,
+		cells: function (row, col, prop) {
+	                if (row === 0) {
+	                  this.renderer = greyRenderer;
+		                }
+	              },
+                  afterChange: function (change, source) {
+                    if (source === 'loadData') {
+                      return; //don't save this change
+                    }
+                    if ($parent.find('input[name=autosave]').is(':checked')) {
+                      clearTimeout(autosaveNotification);
+                      $.ajax({
+                        url: "json/save.json",
+                        dataType: "json",
+                        type: "POST",
+                        data: change, //contains changed cells' data
+                        complete: function (data) {
+                          $console.text('Autosaved (' + change.length + ' ' +
+                            'cell' + (change.length > 1 ? 's' : '') + ')');
+                          autosaveNotification = setTimeout(function () {
+                            $console.text('Changes will be autosaved');
+                          }, 1000);
+                        }
+                      });
+                    }
+                  }
+                });
+                var handsontable = $container.data('handsontable');
+
+                $parent.find('button[name=load]').click(function () {
+                  $.ajax({
+                    url: "json/load.json",
+                    dataType: 'json',
+                    type: 'GET',
+                    success: function (res) {
+			//var arr=res;
+			//$container.handsontable({colHeaders:res.data[0]});
+			//var dat=res.data.splice(0,1);
+			//dat=$.merge(dat,res.data);
+			handsontable.loadData(res.data);
+			plotted(res);                    
+			$console.text('Data loaded');
+			
+                    }
+                  });
+                });
+
+                $parent.find('button[name=save]').click(function () {
+                  $.ajax({
+                    url: "json/save.json",
+                    data: {"data": handsontable.getData()}, //returns all cells' data
+                    dataType: 'json',
+                    type: 'POST',
+                    success: function (res) {
+                      if (res.result === 'ok') {
+                        $console.text('Data saved');
+                      }
+                      else {
+                        $console.text('Save error');
+                      }
+                    },
+                    error: function () {
+                      $console.text('Save error. POST method is not allowed on GitHub Pages. ' +
+                        'Run this example on your own server to see the success message.');
+                    }
+                  });
+                });
+
+                $parent.find('input[name=autosave]').click(function () {
+                  if ($(this).is(':checked')) {
+                    $console.text('Changes will be autosaved');
+                  }
+                  else {
+                    $console.text('Changes will not be autosaved');
+                  }
+                });
---
+++ js/plot.js
@@ -0,0 +1,52 @@
+function plotted(dat){
+var a = new Array();
+for(i=1, l=dat.data.length; i<l; i++ ){
+	var 	obj = {},
+		temp = dat["data"][i];
+		console.log(l);
+	
+	obj["name"] = temp[0];
+	obj["data"] = temp.slice(1, l);
+	a.push(obj);
+	console.log("dat is",JSON.stringify(dat), temp);
+}
+var datar=dat.data[0];
+datar=datar.slice(1,datar.length);
+console.log(datar);
+$('#container').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Marks Attained'
+            },
+            subtitle: {
+                text: 'Source: Aces.com'
+            },
+            xAxis: {
+                categories: datar
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Marks'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series:a
+        });
+}
+
