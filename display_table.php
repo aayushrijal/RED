022101703063fb04306758a59e17cf6834a248ad
shresthaankit7 <shresthaankit7@gmail.com>
2014-06-23 11:50:42 +0545

    file to load for diagram

--- display_table.php
+++ display_table.php
@@ -8,9 +8,11 @@ include('lib/database.init.php');
 	$temp_array = array();
 	$array_index = 0; //initial index for the $array
 	$temp_array_index = 0; //intial index for the $temp_array
-/*database connection
+	$temp_array[$temp_array_index] = NULL;
 
-*/
+//database connection
+
+
 	$conn = mysql_connect($DB_host,$DB_user,$DB_password);
 	if(!$conn)
 		die("ERROR: ".mysql_error());
@@ -19,49 +21,67 @@ include('lib/database.init.php');
 	$retval = mysql_query($sql,$conn);
 		if(!$retval)
 			die("ERROR: ".mysql_error());
-	while($row=mysql_fetch_assoc($retval,MYSQL_ASSOC)){
-		if( check($row['name']) == 0 ){
+	
+	while($row = mysql_fetch_assoc($retval,MYSQL_ASSOC)){
+
+
+		if( $row['name'] == $temp_array[0]){
 			$temp_array_index++;
-		}else{  		//ie yes_no ==1;
-			array_push($temp_array,$row['name']);
-			$temp_array_index++;
-		}
 			$temp_array[$temp_array_index] = $row['marks'];
+		}else{
+			if(empty($temp_array) ){
+			$temp_array_index = 0;
+			$temp_array[$temp_array_index] = $row['name'];
+			$temp_array_index++;
+			$temp_array[$temp_array_index] = $row['marks'];
+			}else{
+				array_push($array,$temp_array);
+				$temp_array_index = 0;
+				$temp_array[$temp_array_index] = $row['name'];
+				$temp_array_index++;
+				$temp_array[$temp_array_index] = $row['marks'];
+			}
+		}	
 	}
+			array_push($array,$temp_array);
 
 
 
+/*
+//make this function inline function
 
-/*make this function inline function*/
-function check($name){		// $name = name obtained from sql query
+
+function check($name){		// $name = name obtained from sql query static $name_set = 0; //to see if name has been set
 	global $array;
 	global $array_index;
 	global $temp_array;
 	global $temp_array_index;
-	if( '$array[$array_index][0]' == $name ){
-		return 1; //return true;
-	}else{
-		array_push($array,$temp_array);
-		$temp_array_index = 0;
-		$temp_array[$temp_array_index] = $name;
-		$temp_array_index = 1;
-		return 0;//return false;
+	if( '$array[$array_index][0]' == $name ){   //same as name
+			;
+	}else{						//not same name
+		insert_name($name);
+		return 1;
 	}
 	
 }
 
 /*Creating temporary ie new multi_indexed array
 
-*/
 
-/*
+
 function insert_name($name){
 	global $temp_array;
 	global $array;
+	global $name_inserted;
+	$temp_array_index = 0;
 	array_push($temp_array,$name);
-	array_push($array,$temp_array);
+	$name_inserted = 1; //to set that name has been inserted
 }
-*/
-	var_dump($array);
+
+*/	
+	array_shift($array);
+	$a = array("data"=>$array);
+	$json_output = json_encode($a);
+	echo $json_output;
 
 	
