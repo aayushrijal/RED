4d25e3217f52b20fbab18218050679e5d0ec2f2b
acesmndr <acesmndr@gmail.com>
2014-08-09 22:56:52 +0545

    New spreadsheet patch plus data sending to php feature added

--- js/marksheet.js
+++ js/marksheet.js
@@ -4,11 +4,12 @@ var dataObtained={
 table_name="ReportCard of "+dataObtained.data[1][0];
 var failFlag=0;		
 var markSheet=new Array();
+markSheet[0]=["S.N.","Subject","Full Marks","Pass Marks","Marks Attained"];
 var numberOfSubjects=dataObtained.data[0].length;
 	for(i=1;i<numberOfSubjects;i++){
-	markSheet[i-1]=[i,dataObtained.data[0][i],100,32,dataObtained.data[1][i]];
-	if(markSheet[i-1][4]<32){
-				markmarkSheet[i-1][4]+='*';
+	markSheet[i]=[i,dataObtained.data[0][i],100,32,dataObtained.data[1][i]];
+	if(Number(markSheet[i][4])<32){
+				markSheet[i][4]+='*';
 				failFlag=1;
 		}	
 	}
@@ -17,5 +18,14 @@ if(failFlag!=1){
 	totalObtained=0;
 	for(i=1;i<numberOfSubjects;i++){totalObtained+=Number(dataObtained.data[1][i])};
 }
-markSheet[numberOfSubjects-1]=[" "," ",(100*(numberOfSubjects-1))," ",totalObtained];
-$container.handsontable.updateSettings({data:markSheet,colHeaders:["S.N.","Subject","Full Marks","Pass Marks","Marks Attained"],rowHeaders:false});
+markSheet[numberOfSubjects]=[" "," ",(100*(numberOfSubjects-1))," ",totalObtained];
+//$container.handsontable.updateSettings({data:markSheet,colHeaders:["S.N.","Subject","Full Marks","Pass Marks","Marks Attained"],rowHeaders:false});
+var dataToPrintFetch;
+var dataToPrint=new Array();
+var fullMarksAttained=0;
+setTimeout(function(){
+	dataToPrintFetch=handsontable.getData();
+	dataToPrint=dataToPrintFetch.slice(1,(dataToPrintFetch.length-2));
+	fullMarksAttained=dataToPrintFetch[dataToPrintFetch.length-1][4];
+},10);
+
--- js/responsive_page.js
+++ js/responsive_page.js
@@ -20,7 +20,9 @@ $("#newMarkSheet").click(function(){
 $("#button1").click(function(){
 	table_name=$("#text1").val();
 	$("#internalFirst").load("internalFirst.html");
+	setTimeout(function(){	
 	handsontable.loadData([["Name","Subject 1","Subject 2","Subject 3","Subject 4","Subject 5"],[],[],[],[],[" "]]);
+	},5);
 });
 $(".icon").click(function(){
 	$("#wholeBody").removeClass("section1");
@@ -29,3 +31,11 @@ $("#import").click(function(){
 	$("#firstPage").hide();
 	$("#internalFirst").load("import.html");	
 });
+$("#downloadPdf").click(function(){
+		$.ajax({
+			url: "download_marksheet.php",
+			dataType: 'json',
+                    	type: 'POST',
+			data: {"data": dataToPrint,"table_name":table_name,"full_marks":fullMarksAttained}
+		});
+	});
